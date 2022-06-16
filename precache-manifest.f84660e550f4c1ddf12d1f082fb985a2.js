db3135d32b6f67b7e6ed00a26113fdd6518ab81f
Roxana38 <98492894+Roxana38@users.noreply.github.com>
2022-06-16 19:14:26 +0300

    Updates

---
+++ precache-manifest.f84660e550f4c1ddf12d1f082fb985a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36f1bc354d0ba3eee69e8671b16f08b",
+    "url": "/Cat-portal-reactjs/index.html"
+  },
+  {
+    "revision": "6c6ec1c14d706c569acd",
+    "url": "/Cat-portal-reactjs/static/css/2.9e880f3b.chunk.css"
+  },
+  {
+    "revision": "6c6ec1c14d706c569acd",
+    "url": "/Cat-portal-reactjs/static/js/2.8eac1f4e.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/Cat-portal-reactjs/static/js/2.8eac1f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee7d81cfd30d3cc7283",
+    "url": "/Cat-portal-reactjs/static/js/main.6ba9d6cd.chunk.js"
+  },
+  {
+    "revision": "632e1bfdae3a3637b499",
+    "url": "/Cat-portal-reactjs/static/js/runtime-main.f6b8da26.js"
+  }
+]);
